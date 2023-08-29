a95868b3989185c849c87df7d0bec500639196a4
gustavomartinezx <gustavopalmr@gmail.com>
2023-08-29 15:25:09 -0300

    exercicios dicionario

--- dicionario.py
+++ dicionario.py
@@ -1,1 +1,20 @@
-testando = 3
+# Métodos úteis dos dicionários em Python
+# len - quantas chaves
+# keys - iterável com as chaves
+# values - iterável com os valores
+# items - iterável com chaves e valores
+# setdefault - adiciona valor se a chave não existe
+# copy - retorna uma cópia rasa (shallow copy)
+# get - obtém uma chave
+# pop - Apaga um item com a chave especificada (del)
+# popitem - Apaga o último item adicionado
+# update - Atualiza um dicionário com outro
+
+pessoa = {
+    'nome': 'Luiz',
+    'sobrenome': 'Otavio',
+    'endereço': 'Manaíra'
+}
+
+print(list(pessoa.keys()))
+print(list(pessoa.values()))
